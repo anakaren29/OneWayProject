4a9e6f5d16ec5a0f51ea3b05cc6efeb4fc654be1
karen <ana_karen_jasso@outlook.com>
2023-03-08 22:22:53 -0600

    change state in button for open the infomation

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,45 @@ import './App.css'
 
 
 function App() {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [selected, setSelected] = useState(null);
+
 
   return (
     <main>
       <h1>Frequently asked questions</h1>
     <Panel titulo={'What is Artlist?'} parrafo={'Artlist is an all-in-one platform for video creators, providing you with the highest quality digital assets made by leading artists worldwide.'}
-       active={activeIndex === 0}
-      onShow = {() => setActiveIndex(0)} 
+       active={selected === 0}
+      onShow = {() => {
+           if(selected === 0){
+             return setSelected(null)
+           }
+           setSelected(0)
+      }} 
       />
     <Panel titulo={'Are Artlist digital assets royalty-free'} parrafo='Artlist’s digital assets are all royalty-free, meaning that you can use them in your videos without having to pay royalty fees.'
-      active={activeIndex === 1}
-      onShow={() => setActiveIndex(1)} />
+      active={selected === 1}
+      onShow = {() => {
+        if(selected === 1){
+          return setSelected(null)
+        }
+        setSelected(1)
+   }}  />
     <Panel titulo={'How do the plans work and what do they cover'} parrafo='Artlist offers a range of plans, depending on the assets and license you need. We offer Music & SFX plans, Footage & Templates plans and Artlist Max — an all-in-one subscription giving you unlimited access to Artlist. '
-      active={activeIndex === 2}
-      onShow={() => setActiveIndex(2)} />
+      active={selected === 2}
+      onShow = {() => {
+        if(selected === 2){
+          return setSelected(null)
+        }
+        setSelected(2)
+   }} />
     <Panel titulo={'Can I try Artlist for free'} parrafo='Yes. With a free account you can access watermarked songs and footage to test in your videos, and save your favorite assets into curated collections.'
-      active={activeIndex === 3}
-      onShow={() => setActiveIndex(3)} />
+      active={selected === 3}
+      onShow = {() => {
+        if(selected === 3){
+          return setSelected(null)
+        }
+        setSelected(3)
+   }}  />
     </main>
   )
 }
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,14 +1,18 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCircleChevronUp, faCircleChevronDown} from '@fortawesome/free-solid-svg-icons';
+
+
 export default function Panel({ titulo, parrafo, active, onShow }) {
 
   return (
      <div>
        <button  onClick={onShow}>
          {titulo}
-         {active ? <div className="chevronActive">&#8964;</div>:
-         <div className="chevronDesactive">&#8964;</div>}
+         { active ? <div > <FontAwesomeIcon icon={faCircleChevronUp} size="xl"/></div>:
+            <div ><FontAwesomeIcon icon={faCircleChevronDown} size="lg"/> </div>}
        </button>
        {
-         active && (<p>{parrafo}</p> ) 
+         active && (<p>{parrafo}</p> )
        }
      </div >
 
